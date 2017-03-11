2b3c7644dfed38b84300a8b1245d8b42d59c853a
Balazs Kozma <kozma.balazs1992@gmail.com>
2017-03-11 02:05:44 +0100

    no message

--- Solution/CowBullGame/main.cpp
+++ Solution/CowBullGame/main.cpp
@@ -1,31 +1,56 @@
 #include <iostream>
 #include <string>
-
-using namespace std;
+#include "FBullCowGame.h"
 
 void PrintIntro();
-string GetGuessAndPrintBack();
+void PlayGame();
+std::string GetGuess();
+bool AskToPlayAgain();
+
+FBullCowGame BCGame;
 
 int main() {
-	PrintIntro();
-	for (int i = 0; i < 5; i++)
-	{
-		GetGuessAndPrintBack();
-	}
-}
+	std::cout << BCGame.GetCurrentTry();
 
-string GetGuessAndPrintBack() {
-	string Guess = "";
-	cout << "Please Type in your guess: ";
-	getline(cin, Guess);
-	cout << "Your guess was: " << Guess << endl << endl;
-	return Guess;
+	do {
+		PrintIntro();
+		PlayGame();
+	} while (AskToPlayAgain());
+
+	return 0;
 }
 
 void PrintIntro() {
 	constexpr int WORD_LENGTH = 10;
-	cout << "Welcome to Cows and Bulls" << endl;
-	cout << "Can you guess the " << WORD_LENGTH;
-	cout << " letter isogram I'm thinkin of?" << endl << endl;
+	std::cout << "Welcome to Cows and Bulls" << std::endl;
+	std::cout << "Can you guess the " << WORD_LENGTH;
+	std::cout << " letter isogram I'm thinkin of?" << std::endl << std::endl;
 	return;
+}
+
+void PlayGame() {
+	BCGame.Reset();
+	int MaxTries = BCGame.GetMaxTries();
+	std::cout << MaxTries << std::endl;
+
+	for (int i = 0; i < MaxTries; i++)
+	{
+		std::string Guess = GetGuess();
+		std::cout << "Your guess was: " << Guess << std::endl << std::endl;
+	}
+}
+
+std::string GetGuess() {
+	int CurrentTry = BCGame.GetCurrentTry();
+	std::string Guess = "";
+	std::cout << "Try " << CurrentTry << ". Please Type in your guess: ";
+	std::getline(std::cin, Guess);
+	return Guess;
+}
+
+bool AskToPlayAgain() {
+	std::cout << "Do you want to play again?";
+	std::string Response = "";
+	std::getline(std::cin, Response);
+	return (Response[0] == 'y' || Response[0] == 'Y');
 }
